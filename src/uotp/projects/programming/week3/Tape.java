2848fa7392de798b5d007f3392bda147c03b355a
Alex <alexwilkinson@gmail.com>
2016-09-21 09:22:11 +0800

    Sept 21 - all files put in week 3 and new tape class created

---
+++ src/uotp/projects/programming/week3/Tape.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uotp.projects.programming.week3;
+
+/**
+ *
+ * @author Alex
+ */
+public class Tape {
+    
+    public Tape(){
+        
+    }
+    
+    public Cell getCurrentCell(){
+        
+        return null;
+    }
+    
+    public char getContent(){
+        
+        return 'c';
+    }
+    
+    public void setContent(char ch){
+        
+        
+    }
+    
+    public void moveLeft(){
+        
+    }
+    
+    public void moveRight(){
+        
+    }
+    
+    public String getTapeContents(){
+        
+        return null;
+    }
+        
+    
+}
